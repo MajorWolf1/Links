e6d9914e7e33c199aa7d6a98508f81036b5ff1ed
MajorWolf1 <mitchellperrie@hotmail.co.uk>
2022-03-14 16:27:15 +0000

    Update items.js

--- src/assets/items.js
+++ src/assets/items.js
@@ -1,60 +1,61 @@
-import website from '../assets/website.png'
-import instagram from '../assets/instagram.png'
-import appstore from '../assets/playstore.png'
-import youtube from '../assets/youtube.jpeg'
-import dribbble from '../assets/dribbble.png'
-import github from '../assets/github.png'
 import telegram from '../assets/telegram.png'
+import instagram from '../assets/instagram.png'
+import snapchat from '../assets/snapchat.png'
+import tiktok from '../assets/tiktok.png'
 import twitter from '../assets/twitter.png'
+import xbox from '../assets/xbox.png'
+import psn from '../assets/psn.png'
+import steam from '../assets/steam.png'
+
+
 
 const items = [{
-    "title": "Website",
-    "subtitle": "Look at my work!",
-    "image": website,
-    "link": "https://sagniksahoo.codes" //your personal website or portfolio  link
-},
-{
-    "title": "GitHub",
-    "subtitle": "@heysagnik | 🏠 of my open-source projects",
-    "image": github,
-    "link": "https://github.com/heysagnik" //Github Profile link
+    "title": "Telegram",
+    "subtitle": "Chat with me instantly, Don't hesitate! ",
+    "image": telegram,
+    "link": "https://telegram.me/MitchellP" //Telegram Profile 
 },
 {
     "title": "Instagram",
-    "subtitle": "@heysagnik | Hub of my awesome 😎 pics",
+    "subtitle": "Hub of my awesome 😎 pics",
     "image": instagram,
-    "link": "https://instagram.com/heysagnik" //instagram profile link 
+    "link": "https://www.instagram.com/mitchp3598/" //Instagram Profile Link 
+},
+{
+    "title": "Snapchat",
+    "subtitle": "🏠 of my open-source projects",
+    "image": snapchat,
+    "link": "https://github.com/heysagnik" //Snapchat Profile Link
+},
+{
+    "title": "Tiktok",
+    "subtitle": "My tiktok",
+    "image": tiktok,
+    "link": "https://www.tiktok.com/@mitchyp69" //Tiktok Profile Link
 },
 {
     "title": "Twitter",
-    "subtitle": "@heysagnik | Don't forget to follow me 😉",
+    "subtitle": "Don't forget to follow me 😉",
     "image": twitter,
-    "link": "https://twitter.com/heysagnik"// twitter profile link 
+    "link": "https://twitter.com/CheshireCat_exe"//Twitter Profile Link 
 },
 {
-    "title": "Apps",
-    "subtitle": "Hub of my awesome 🔥 Apps",
-    "image": appstore,
-    "link": "#"//
+    "title": "XBOX",
+    "subtitle": "AMFMitch9106",
+    "image": xbox,
+    "link": "http://live.xbox.com/Profile?Gamertag=AMFMitch9106"//XBOX Profile Link
 },
 {
-    "title": "YouTube",
-    "subtitle": "CODER SAGNIK | Official channel of mine",
-    "image": youtube,
-    "link": "https://www.youtube.com/channel/UCOUa9hvd4sJWQWQRIQctbSg"//youtube channel link 
-},
-
-{
-    "title": "Dribbble",
-    "subtitle": "@virtuonic | Hub to the Shots of my 😎 apps ",
-    "image": dribbble,
-    "link": "https://dribbble.com/virtuonic" // Dribbble profile link 
+    "title": "PSN",
+    "subtitle": "AMFMitch",
+    "image": xbox,
+    "link": "#"//PSN Profile Link
 },
 {
-    "title": "Telegram",
-    "subtitle": "@heysagnik | Chat with me instantly, Don't hesitate! ",
-    "image": telegram,
-    "link": "https://telegram.me/heysagnik" //Telegram Pofile 
+    "title": "Steam",
+    "subtitle": "SonOfAMitch",
+    "image": steam,
+    "link": "https://steamcommunity.com/id/MitchP98/"//Steam Channel Link 
 }]
 
 export default items
